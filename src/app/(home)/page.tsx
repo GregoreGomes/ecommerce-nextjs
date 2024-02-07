8f3a4e972fee7601dad04ead35996fe7cd1e3bf4
Gregore Gomes <gregore994@gmail.com>
2024-02-07 00:36:01 -0300

    m tailwind add

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import Image from "next/image";
+
+import { Button } from "@material-tailwind/react";
+
 
 export default function Home() {
   return (
+    <>
     <main className="flex min-h-screen flex-col p-24">
       <h1>projeto</h1>
+      <Button placeholder={'teste'}>Button</Button>
     </main>
+    </>
   );
 }
